93c15555160e5adf1d74be861424415ccfed2c9b
SHUBHAM SONI <shubsoni734@gmail.com>
2023-05-02 23:51:16 +0530

    create navbar
    create nav bar and connect to routes.

--- shreeji/src/App.js
+++ shreeji/src/App.js
@@ -1,14 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Layout from "./Components/Layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import HomePages from "./Pages/HomePages";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Policy from "./Pages/Policy";
+import PageeNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <h1>Hello shree ji</h1>
-      </Layout>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Policy" element={<Policy />} />
+        <Route path="*" element={<PageeNotFound />} />
+      </Routes>
+    </>
   );
 }
 
--- shreeji/src/Pages/Policy.js
+++ shreeji/src/Pages/Policy.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Layout from "../Components/Layout/Layout";
 
 const Policy = () => {
   return (
     <Layout>
-      <h1>Policy</h1>
+      <h1>Privacy Policy</h1>
     </Layout>
   );
 };
