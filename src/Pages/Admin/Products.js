3408b32557c58ca2fb42fe067082f18a94ea430c
SHUBHAM SONI <shubsoni734@gmail.com>
2023-06-30 22:56:24 +0530

    w

--- src/Pages/Admin/Products.js
+++ src/Pages/Admin/Products.js
@@ -17,7 +17,7 @@ const Products = () => {
   }, []);
   return (
     <Layout>
-      <div className="container-fluid dashboard m-3 p-3">
+      <div className="container-fluid m-3 p-3">
         <div className="row">
           <div className="col-md-3">
             <AdminMenu />
